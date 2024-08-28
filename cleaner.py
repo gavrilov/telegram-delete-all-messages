46c8143411bc50cbf3c8eeb839a3e1bf38f1cdda
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-08-27 19:28:52 -0700

    reformat code

--- cleaner.py
+++ cleaner.py
@@ -7,6 +7,7 @@ from pyrogram.client import Client
 from pyrogram.errors import FloodWait
 from pyrogram.errors.rpc_error import UnknownError
 
+
 class Cleaner:
     def __init__(self, chats=None, search_chunk_size=100, delete_chunk_size=100):
         self.chats = chats or []
@@ -16,16 +17,17 @@ class Cleaner:
             # The issue is that pyrogram.raw.functions.messages.Search uses
             # pagination with chunks of 100 messages. Might consider switching
             # to search_messages, which handles pagination transparently.
-            raise ValueError('search_chunk_size > 100 not supported')
+            raise ValueError("search_chunk_size > 100 not supported")
         self.search_chunk_size = search_chunk_size
         self.delete_chunk_size = delete_chunk_size
 
     @staticmethod
     def chunks(l, n):
         """Yield successive n-sized chunks from l.
-        https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks#answer-312464"""
+        https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks#answer-312464
+        """
         for i in range(0, len(l), n):
-            yield l[i:i + n]
+            yield l[i : i + n]
 
     @staticmethod
     async def get_all_chats():
@@ -36,38 +38,38 @@ class Cleaner:
 
     async def select_groups(self, recursive=0):
         chats = await self.get_all_chats()
-        groups = [c for c in chats if c.type.name in ('GROUP, SUPERGROUP')]
+        groups = [c for c in chats if c.type.name in ("GROUP, SUPERGROUP")]
 
-        print('Delete all your messages in')
+        print("Delete all your messages in")
         for i, group in enumerate(groups):
-            print(f'  {i+1}. {group.title}')
+            print(f"  {i+1}. {group.title}")
 
         print(
-            f'  {len(groups) + 1}. '
-            '(!) DELETE ALL YOUR MESSAGES IN ALL OF THOSE GROUPS (!)\n'
+            f"  {len(groups) + 1}. "
+            "(!) DELETE ALL YOUR MESSAGES IN ALL OF THOSE GROUPS (!)\n"
         )
 
-        nums_str = input('Insert option numbers (comma separated): ')
-        nums = map(lambda s: int(s.strip()), nums_str.split(','))
+        nums_str = input("Insert option numbers (comma separated): ")
+        nums = map(lambda s: int(s.strip()), nums_str.split(","))
 
         for n in nums:
             if not 1 <= n <= len(groups) + 1:
-                print('Invalid option selected. Exiting...')
+                print("Invalid option selected. Exiting...")
                 exit(-1)
 
             if n == len(groups) + 1:
-                print('\nTHIS WILL DELETE ALL YOUR MESSSAGES IN ALL GROUPS!')
+                print("\nTHIS WILL DELETE ALL YOUR MESSSAGES IN ALL GROUPS!")
                 answer = input('Please type "I understand" to proceed: ')
-                if answer.upper() != 'I UNDERSTAND':
-                    print('Better safe than sorry. Aborting...')
+                if answer.upper() != "I UNDERSTAND":
+                    print("Better safe than sorry. Aborting...")
                     exit(-1)
                 self.chats = groups
                 break
             else:
                 self.chats.append(groups[n - 1])
-        
-        groups_str = ', '.join(c.title for c in self.chats)
-        print(f'\nSelected {groups_str}.\n')
+
+        groups_str = ", ".join(c.title for c in self.chats)
+        print(f"\nSelected {groups_str}.\n")
 
         if recursive == 1:
             self.run()
@@ -90,7 +92,7 @@ class Cleaner:
             await self.delete_messages(chat_id=chat.id, message_ids=message_ids)
 
     async def delete_messages(self, chat_id, message_ids):
-        print(f'Deleting {len(message_ids)} messages with message IDs:')
+        print(f"Deleting {len(message_ids)} messages with message IDs:")
         print(message_ids)
         for chunk in self.chunks(message_ids, self.delete_chunk_size):
             try:
@@ -99,13 +101,15 @@ class Cleaner:
             except FloodWait as flood_exception:
                 sleep(flood_exception.x)
 
-
     async def search_messages(self, chat_id, add_offset):
         messages = []
-        print(f'Searching messages. OFFSET: {add_offset}')
-        async for message in app.search_messages(chat_id=chat_id, offset=add_offset, from_user="me", limit=100):
+        print(f"Searching messages. OFFSET: {add_offset}")
+        async for message in app.search_messages(
+            chat_id=chat_id, offset=add_offset, from_user="me", limit=100
+        ):
             messages.append(message)
         return messages
+
 
 async def main():
     try:
@@ -114,10 +118,11 @@ async def main():
             await deleter.select_groups()
             await deleter.run()
     except UnknownError as e:
-        print(f'UnknownError occurred: {e}')
-        print('Probably API has changed, ask developers to update this utility')
+        print(f"UnknownError occurred: {e}")
+        print("Probably API has changed, ask developers to update this utility")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cachePath = os.path.abspath(__file__)
     cachePath = os.path.dirname(cachePath)
     cachePath = os.path.join(cachePath, "cache")
@@ -125,12 +130,14 @@ if __name__ == '__main__':
     if os.path.exists(cachePath):
         with open(cachePath, "r") as cacheFile:
             cache = json.loads(cacheFile.read())
-        
+
         API_ID = cache["API_ID"]
         API_HASH = cache["API_HASH"]
     else:
-        API_ID = os.getenv('API_ID', None) or int(input('Enter your Telegram API id: '))
-        API_HASH = os.getenv('API_HASH', None) or input('Enter your Telegram API hash: ')
+        API_ID = os.getenv("API_ID", None) or int(input("Enter your Telegram API id: "))
+        API_HASH = os.getenv("API_HASH", None) or input(
+            "Enter your Telegram API hash: "
+        )
 
     app = Client("client", api_id=API_ID, api_hash=API_HASH)
 
