7de79bc011f19d1458b9936ac0d4f1e43d400f34
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-08-27 20:43:40 -0700

    format the code

--- rm_reactions.py
+++ rm_reactions.py
@@ -7,14 +7,15 @@ from telethon.tl.types import Channel, Chat, PeerUser
 from pyrogram.client import Client
 
 
-API_ID = os.getenv('API_ID', None) or int(input('Enter your Telegram API id: '))
-API_HASH = os.getenv('API_HASH', None) or input('Enter your Telegram API hash: ')
+API_ID = os.getenv("API_ID", None) or int(input("Enter your Telegram API id: "))
+API_HASH = os.getenv("API_HASH", None) or input("Enter your Telegram API hash: ")
 
-# telethon and pyrogram have different session file formats - because of this the script created 2 sessions and you had to log in twice
-session_name_pyrogram ='rm_reactions_pyrogram'
-session_name_telethon = 'rm_reactions_telethon'
+# telethon and pyrogram have different session file formats, because rm_reactions.py creates 2 sessions and you have to log in twice
+session_name_pyrogram = "rm_reactions_pyrogram"
+session_name_telethon = "rm_reactions_telethon"
 client = TelegramClient(session_name_telethon, API_ID, API_HASH)
 app = Client(session_name_pyrogram, api_id=API_ID, api_hash=API_HASH)
+
 
 async def rm_reaction(app, chat_id, msg_id):
     try:
@@ -22,10 +23,16 @@ async def rm_reaction(app, chat_id, msg_id):
     except Exception as e:
         print("Failed to remove reaction:", e)
 
+
 async def main():
     async with app:
         parser = argparse.ArgumentParser(description="Process some flags.")
-        parser.add_argument('-l', '--list', action='store_true', help='list chats with reactions but do not delete')
+        parser.add_argument(
+            "-l",
+            "--list",
+            action="store_true",
+            help="list chats with reactions but do not delete",
+        )
 
         args = parser.parse_args()
 
@@ -34,10 +41,12 @@ async def main():
 
         dialogs = await client.get_dialogs()
         groups = []
-        
+
         for dialog in dialogs:
             entity = dialog.entity
-            if isinstance(entity, Chat) or (isinstance(entity, Channel) and entity.megagroup):
+            if isinstance(entity, Chat) or (
+                isinstance(entity, Channel) and entity.megagroup
+            ):
                 groups.append(dialog)
 
         if not groups:
@@ -49,9 +58,11 @@ async def main():
             print(f"{i}. {group.name}")
         print(f"{len(groups)}. ALL GROUPS")
 
-        selected_group_indexs = input("Select one or many groups by entering their indexes comma separated: ")
+        selected_group_indexs = input(
+            "Select one or many groups by entering their indexes comma separated: "
+        )
 
-        str_list = selected_group_indexs.split(',')
+        str_list = selected_group_indexs.split(",")
         int_list = [int(num.strip()) for num in str_list]
 
         groups_to_process = []
@@ -64,7 +75,6 @@ async def main():
         print(f"\nGroups to Process:")
         for g in groups_to_process:
             print(f"\t{g.name}")
-
 
         for g in groups_to_process:
             print(f"\nProcessing {g.name}")
@@ -80,7 +90,10 @@ async def rm_reactions_from_chat(app, chat_id, me, rm: bool = False):
     numMessages = await app.get_chat_history_count(chat_id)
     i = 0
     async for message in client.iter_messages(chat_id):
-        print(f"Progress: {i + 1}/{numMessages} ({(i + 1) / numMessages * 100:.2f}%)", end='\r')
+        print(
+            f"Progress: {i + 1}/{numMessages} ({(i + 1) / numMessages * 100:.2f}%)",
+            end="\r",
+        )
         if message.reactions:
             # print(f'message.reactions: {message.reactions}')
             recent_reactions = message.reactions.recent_reactions
@@ -94,9 +107,10 @@ async def rm_reactions_from_chat(app, chat_id, me, rm: bool = False):
                     if rm:
                         await rm_reaction(app, chat_id, message.id)
                         print(f"Removed reaction on msg_id: {message.id}")
-                    reactionCount+=1
+                    reactionCount += 1
         i += 1
     return reactionCount
+
 
 with client:
     client.loop.run_until_complete(main())
